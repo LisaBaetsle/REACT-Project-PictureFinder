a49363530d20b87784d6901b0a3d17ea8de3fb2b
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-11-19 10:43:51 +0100

    handling user input with forms and events

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
     <div className="ui container" style={{ marginTop: '30px' }}>
-      <SearchBar />
+      <SearchBar whenSubmit={this.onSearchSubmit}/>
     </div>
-  )
-};
+  );
+  }
+}
 
 export default App;
